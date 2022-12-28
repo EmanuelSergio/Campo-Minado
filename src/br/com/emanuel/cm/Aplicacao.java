b5aae0d837130980d8f9b46aed1bdd380ea59bec
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2022-12-28 01:00:06 -0300

    update

--- src/br/com/emanuel/cm/Aplicacao.java
+++ src/br/com/emanuel/cm/Aplicacao.java
@@ -10,7 +10,9 @@ public static void main(String[] args) {
 	Tabuleiro t = new Tabuleiro(6, 6, 6);
 	new TabuleiroConsole(t);
 	
-	System.out.println("funcionau");
+	System.out.println("funcionauddd");
+	
+	
 	
 }
 }
